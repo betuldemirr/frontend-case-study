b67520b17681160abb7ac3ec8ffb9f957b1cbbff
betuldemirr <betuldemir.007@gmail.com>
2024-05-14 21:06:27 +0300

    init

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetail = () => {
+    return (
+        <div className="product-detail">
+            Product Detail
+        </div>
+    );
+}
+
+export default ProductDetail;
---
+++ src/pages/ProductList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductList = () => {
+    return (
+        <div className="product-list">
+            Product List
+        </div>
+    );
+}
+
+export default ProductList;
