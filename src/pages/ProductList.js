a80243a1f62362c9d6bc769cfca46be7809b53d1
betuldemirr <betuldemir.007@gmail.com>
2024-05-17 08:33:35 +0300

    created pagination

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Button, Col, Row } from 'react-bootstrap';
+import { Card, Button, Col, Row, Pagination} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { getAllProducts } from '../services/api';
 import { addToCart } from '../features/cartSlice';
@@ -7,21 +7,29 @@ import { useDispatch } from 'react-redux';
 
 const ProductList = ({ searchTerm }) => {
     const navigate = useNavigate();
-    const [products, setProducts] = useState([]);
     const dispatch = useDispatch();
+    const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const productsPerPage = 12;
 
     useEffect(() => {
-        const fetchProducts = async () => {
+        setCurrentPage(1);
+        const filterProducts = async () => {
             try {
-                const products = await getAllProducts();
-                setProducts(products);
+                const allProducts = await getAllProducts();
+                
+                const filteredProducts = searchTerm
+                    ? allProducts.filter(product => product.name.toLowerCase().includes(searchTerm.toLowerCase()))
+                    : allProducts;
+                console.log("Filtered Products:", filteredProducts);
+                setProducts(filteredProducts);
             } catch (error) {
-                console.error('An error occurred while fetching the products:', error);
+                console.error('An error occurred while filtering the products:', error);
             }
         };
-        fetchProducts();
-    }, []);
-
+        filterProducts();
+    }, [searchTerm]);
+    
     useEffect(() => {
         const filterProducts = async () => {
             try {
@@ -38,30 +46,49 @@ const ProductList = ({ searchTerm }) => {
         filterProducts();
     }, [searchTerm]);
 
+    const totalProducts = products.length;
+    const totalPages = Math.ceil(totalProducts / productsPerPage);
+
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+    
+    const onPaginationClick = (page) => {
+        setCurrentPage(page);
+    };
+
     const goToProductDetail = (productId) => {
         navigate(`/product-details/${productId}`);
     };
 
     return (
-        <div className="product-list bg-info">
-            <h2 className="text-center mb-4">Products</h2>
+        <div className="product-list my-4">
             <Row xs={1} md={2} lg={4} className="g-4">
-                {products.map(product => (
+                {currentProducts.map(product => (
                     <Col key={product.id}>
                         <Card>
                             <Card.Body onClick={() => goToProductDetail(product.id)} style={{ cursor: 'pointer' }}>
-                                <Card.Img variant="top" src={product.image} />
+                                <Card.Img className='pb-4' variant="top" src={product.image} />
                                 <Card.Title>{product.name}</Card.Title>
                             </Card.Body>
                             <Card.Footer>
-                                <Card.Text>{product.price} ₺</Card.Text>
-                                <Button onClick={() => dispatch(addToCart(product))} variant="success" className="w-100">
+                                <Card.Text className='text-primary fw-bold'>{product.price} ₺</Card.Text>
+                                <Button onClick={() => dispatch(addToCart(product))} variant="primary" className="w-100">
                                     Add to Cart
                                 </Button>
                             </Card.Footer>
                         </Card>
                     </Col>
                 ))}
+            </Row>
+            <Row className="my-4">
+                <Pagination className='d-flex justify-content-center'>
+                    {Array.from({ length: totalPages }, (_, i) => (
+                        <Pagination.Item key={i + 1} active={i + 1 === currentPage} onClick={() => onPaginationClick(i + 1)}>
+                            {i + 1}
+                        </Pagination.Item>
+                    ))}
+                </Pagination>
             </Row>
         </div>
     );
